761dbf28f9677e34b739d7547c80955f200ba1b1
YongJun Kwon <dydwns18011@gmail.com>
2023-02-12 19:31:54 +0900

    websocket 적용

---
+++ src/main/java/com/chat/config/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package com.chat.config;
+
+import com.chat.handler.SocketHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Autowired
+    SocketHandler socketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(socketHandler, "/api/chat").setAllowedOrigins("*");
+    }
+}
